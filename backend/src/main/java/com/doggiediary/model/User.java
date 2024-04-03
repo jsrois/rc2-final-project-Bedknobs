d3167272613287a1bed0f62da2fc8aff24b96403
Bedknobs <noeliareysilva@gmail.com>
2024-04-03 18:10:21 +0200

    Created backend configuration and model started

---
+++ backend/src/main/java/com/doggiediary/model/User.java
@@ -0,0 +1,29 @@
+package com.doggiediary.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    private String fullName;
+    private String location;
+    private String birthDate;
+    private String email;
+    private String password;
+    private String mobile;
+    private String image;
+    private String backgroundImage;
+    private String bio;
+    private boolean req_user;
+    private boolean login_with_google;
+
+}
