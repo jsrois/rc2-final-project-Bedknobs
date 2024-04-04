1c784a3f30839fa6758efcd4a59c2a41b3f585eb
Bedknobs <noeliareysilva@gmail.com>
2024-04-04 17:01:13 +0200

    Tests of entries and posts done

---
+++ frontend/src/test/EntriesAndPost.test.jsx
@@ -0,0 +1,54 @@
+import { expect, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Entries from "../components/Entries/Entries";
+
+test("Aparece el nombre de la persona que publicó la entrada", () => {
+  try {
+    render(<Entries />);
+    expect(screen.queryByText("PUBLICAR")).toBeInTheDocument();
+  } catch (error) {
+    console.error("Error en la prueba:", error);
+  }
+});
+
+test("Los posts se muestran correctamente", async () => {
+  try {
+    render(<Entries />);
+    await screen.findByText("Maravillosa vista");
+    expect(screen.queryByText("Maravillosa vista")).toBeInTheDocument();
+  } catch (error) {
+    console.error("Error en la prueba:", error);
+  }
+});
+
+test("Subir una foto abre el diálogo de selección de archivos", async () => {
+  try {
+    render(<Entries />);
+    const fileInput = screen.getByLabelText("Subir foto");
+    fireEvent.change(fileInput, {
+      target: {
+        files: [new File(["perro"], "perro.png", { type: "image/png" })],
+      },
+    });
+    const selectedImage = await screen.findByAltText("Imagen seleccionada");
+    expect(selectedImage).toBeInTheDocument();
+  } catch (error) {
+    console.error("Error en la prueba:", error);
+  }
+});
+
+test("Hacer clic en el botón de opciones muestra las opciones de edición", async () => {
+  try {
+    render(<Entries />);
+    expect(screen.queryByText("Borrar")).not.toBeInTheDocument();
+    expect(screen.queryByText("Editar")).not.toBeInTheDocument();
+
+    const button = screen.getByRole("button", { name: "Opciones" });
+    fireEvent.click(button);
+
+    expect(await screen.findByText("Borrar")).toBeInTheDocument();
+    expect(await screen.findByText("Editar")).toBeInTheDocument();
+  } catch (error) {
+    console.error("Error en la prueba:", error);
+  }
+});
